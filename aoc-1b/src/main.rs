f9b1e29a009311a561447054d74093967a8275f6
Clover Fox <cloverfox@protonmail.ch>
2022-12-06 01:35:17 +0000

    added day 1b

---
+++ aoc-1b/src/main.rs
@@ -0,0 +1,20 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("input.txt")
+        .expect("could not load file for some raisen");
+
+    let mut elf_calories = input.split("\n\n")
+        .map(|elf| elf.lines()
+             .map(|calorie| calorie.trim().parse::<i32>().unwrap())
+             .fold(0, |acc, x| acc + x))
+        .collect::<Vec<i32>>();
+
+    elf_calories.sort_by(|a, b| b.cmp(a)); 
+
+    let max_three_elves = elf_calories.iter()
+        .take(3)
+        .fold(0, |acc, x| acc + x);
+
+    println!("the max calorie count is {}", max_three_elves);
+}
